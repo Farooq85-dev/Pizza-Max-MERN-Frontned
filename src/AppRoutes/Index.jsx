560a0d06c292bcb86696110c9709176e9b363594
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-12-12 20:36:05 +0500

    user dashboard initialized

--- src/AppRoutes/Index.jsx
+++ src/AppRoutes/Index.jsx
@@ -3,9 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Loader from "../Components/Loader";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
-const HomePage = lazy(() => import("../Pages/Home"));
+const Home = lazy(() => import("../Pages/Home"));
 const Checkout = lazy(() => import("../Pages/Checkout"));
 const NotFound = lazy(() => import("../Pages/NotFound"));
+const UserDashboard = lazy(() => import("../Pages/UserDashboard"));
 
 const AppRouting = () => {
   return (
@@ -13,8 +14,9 @@ const AppRouting = () => {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route index element={<HomePage />} />
+          <Route index element={<Home />} />
           <Route path="/checkout" element={<Checkout />} />
+          <Route path="/user" element={<UserDashboard />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
