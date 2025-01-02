680247a6920ce31bf248860e5d5dd7b25068ec69
Muhammad Farooq <irfankhans786787788@gmail.com>
2025-01-02 11:06:22 +0500

    admi users status icon changed and size is updated

--- src/Components/Admin/Users.jsx
+++ src/Components/Admin/Users.jsx
@@ -1,8 +1,9 @@
-import { LuUsersRound } from "react-icons/lu";
+import { LuUserSearch, LuUsersRound } from "react-icons/lu";
 import UsersStatusCard from "../StatusCard";
 import { useUsers } from "../../Context/Admin/Users.context";
 import Table from "../Table";
 import { Userscolumns } from "./Static/UsersColumns";
+import { FaUserSlash } from "react-icons/fa";
 
 const UsersComp = () => {
   const {
@@ -19,17 +20,17 @@ const UsersComp = () => {
       <div className="users-stauts-cartd-container grid grid-col-1 md:grid-cols-3 gap-4 mb-4">
         <UsersStatusCard
           title="Total Users"
-          icon={<LuUsersRound />}
+          icon={<LuUsersRound size={20} />}
           quantity={howMuchTotalUsers}
         />
         <UsersStatusCard
           title="Verified Users"
-          icon={<LuUsersRound />}
+          icon={<LuUserSearch size={20} />}
           quantity={howMuchVerifiedUsers?.length}
         />
         <UsersStatusCard
           title="Un-Verified Users"
-          icon={<LuUsersRound />}
+          icon={<FaUserSlash size={20} />}
           quantity={howMuchUnVerifiedUsers?.length}
         />
       </div>
